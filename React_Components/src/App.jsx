3391e7867d62886db648552ad189f68946451c48
Mubashir Malik <mubashirmalik1616@gmail.com>
2026-02-18 23:50:04 +0500

    Initial commit

---
+++ React_Components/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "./components/Card";
+
+const App = () => {
+  return (
+    <div>
+      {/* Card Component in components Folder */}
+      <Card />
+    </div>
+  );
+};
+
+export default App;
---
+++ React_Components/src/components/Card.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+{
+  /* Card Component Function */
+}
+
+const Card = () => {
+  return (
+    <div className="card">
+      <h2 className="">Basic</h2>
+      <ul className="list">
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+      </ul>
+      <button className="btn">Subscribe</button>
+    </div>
+  );
+};
+
+{
+  /* Exporting Card Component */
+}
+export default Card;
